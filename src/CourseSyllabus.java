7fb5ef905bbe856703c11cba2f756c7c2b8bc231
BhargavRamThatikonda <bthatiko@asu.edu>
2022-10-12 20:41:56 -0700

    builder java file by bhargavram

---
+++ src/CourseSyllabus.java
@@ -0,0 +1,19 @@
+public class CourseSyllabus implements  GenerateSyllabus{
+
+	public void getPreviousSyllabus() {
+
+	}
+
+	public void getCourseTemplate() {
+
+	}
+
+	public void generateSyllabus() {
+
+	}
+
+	public SyllabusGeneration getSyllabus() {
+		return null;
+	}
+
+}
---
+++ src/GenerateSyllabus.java
@@ -0,0 +1,15 @@
+public interface GenerateSyllabus
+{
+
+	public void getPreviousSyllabus();
+
+	public void getCourseTemplate();
+
+	public void generateSyllabus();
+
+	/**
+	 *  
+	 */
+	public SyllabusGeneration getSyllabus();
+
+}
